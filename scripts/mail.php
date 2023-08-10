0e884ded90ad92f99ea45d80034b9867089a9364
SGVcode <sgvero.box@gmail.com>
2023-08-10 20:30:00 -0300

    Añadido a mail.php el informe de error

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recopilar los datos del formulario y sanitizarlos
     $nombre = sanitizeInput($_POST["nombre"]);

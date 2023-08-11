61ecd7727a0a0f139403d4f3949bcea69e392ac0
SGVcode <sgvero.box@gmail.com>
2023-08-11 15:03:29 -0300

    modificado botón arriba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,27 @@
+// Botón Ir Arriba
+document.getElementById("boton-arriba").addEventListener("click", scrollUp);
+
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0, currentScroll - (currentScroll / 10));
+        botonArriba.style.transform = "scale(0)";
+    }
+}
+
+botonArriba = document.getElementById("boton-arriba");
+window.onscroll = function () {
+    let scroll = document.documentElement.scrollTop;
+    if (scroll > 50) {
+        botonArriba.style.transform = "scale(1)";
+    } else {
+        botonArriba.style.transform = "scale(0)";
+    }
+}
+
+
+// Formulario Contacto
 const iconoBurger = document.getElementById('icono-burger');
 const menu = document.getElementById('mostrar-menu');
 
@@ -14,35 +38,17 @@ document.getElementById('form-link').addEventListener('click', function (event) 
     document.getElementById('form-modal').style.display = 'flex';
 });
 
-document.getElementById('form-close').addEventListener('click', function () {
-    document.getElementById('form-modal').style.display = 'none';
-    resetForm();
-});
-
-document.querySelector('#form-modal input[type="submit"]').addEventListener('click', function () {
-    resetForm();
-});
-
 function resetForm() {
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
 }
 
-// Boton Ir Arriba
-document.addEventListener("scroll", function () {
-    var scrollButton = document.getElementById("irArriba");
-    if (window.scrollY >= 20) {
-        scrollButton.style.opacity = "1";
-    } else {
-        scrollButton.style.opacity = "0";
-    }
+document.getElementById('form-close').addEventListener('click', function () {
+    document.getElementById('form-modal').style.display = 'none';
+    resetForm();
 });
 
-// Animación suave para desplazarse hacia arriba al hacer clic en el botón
-document.getElementById("irArriba").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
+// document.querySelector('#form-modal input[type="submit"]').addEventListener('click', function () {
+//     resetForm();
+// });
+
